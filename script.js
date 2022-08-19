d140e62cfe466ba46822ae9df8ca890f770d2e7e
erickssguerra <erickssguerra@gmail.com>
2022-08-19 17:15:26 -0300

    Improved card flipping behavior

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ function shuffleCards() {
         cards.push(i, i); //adicionar de 2 em 2 cartas do mesmo tipo na array
     }
     //depois embaralha os elementos da array de acordo com a função "comparador"
-    // cards.sort(comparador);
+    cards.sort(comparador);
 
 }
 
@@ -47,45 +47,98 @@ function displayCards() { //dispor carta a carta na tela
         const cardDiv = document.createElement("div");
         cardDiv.classList.add("card");
 
-        container.appendChild(cardDiv) //adicionando uma card dentro do container
+        const innerCardDiv = document.createElement("div");
+        innerCardDiv.classList.add("inner-card");
+        cardDiv.appendChild(innerCardDiv)
+
+        container.appendChild(cardDiv) //adicionando div filha chamada cardDiv dentro do container
 
 
         //colocar um atributo na div card chamado position cujo valor é i, isto é, a posição no container
         //este valor vai dar a identidade da carta (que será usada para futura comparação)
-        cardDiv.setAttribute("data-position", i)
+        cardDiv.setAttribute("data-position", i);
+        cardDiv.setAttribute("onclick", "clickCard(this)");
 
 
         //atribuindo a figura do papagaio estático à frente da carta 
         const cardFront = document.createElement("div");
-        cardFront.classList.add("front-card");
-        cardDiv.appendChild(cardFront);
+        cardFront.classList.add("front-card", "face");
+        const parrotFront = document.createElement("img");
+        parrotFront.src = `./images/front.png`;
+        cardFront.appendChild(parrotFront);
+        innerCardDiv.appendChild(cardFront);
 
 
         //atribuindo o gif do papagaio à parte de trás da carta
         const cardBack = document.createElement("div");
-        cardBack.classList.add("hidden", "back-card");
+        cardBack.classList.add("back-card", "face");
         const parrot = document.createElement("img"); //cria a tag <img> que vai receber...
         parrot.src = `./images/parrot${cards[i]}.gif`; //...cards[i] == valor contido no spot[i] (que já foi embaralhado)
         cardBack.appendChild(parrot);
-        cardDiv.appendChild(cardBack);
-
-        //quando clicar essa carta, eu quero que a função showCard seja executada
-
-        cardDiv.onclick = showCard;
-
+        innerCardDiv.appendChild(cardBack);
     }
 }
 
+function flipCard(position) {
+    const inner = document.querySelectorAll(".inner-card")[position]
+    inner.classList.toggle("flip")
+}
+
+function isFlipped(position) {
+    const inner = document.querySelectorAll(".inner-card")[position]
+    return inner.classList.contains("flip")
+}
+
+let lastCard = -1
+
+function clickCard(element) {
+    const position = element.getAttribute("data-position")
+
+    if (isFlipped(position))
+        return
+
+    flipCard(position, false)
+
+    if (lastCard !== -1 && position !== lastCard) {
+
+        if (cards[position] == cards[lastCard]) {
+            console.log("acertou")
+
+        } else {
+            const currentLastCard = lastCard
+            setTimeout(() => {
+                flipCard(position);
+                flipCard(currentLastCard);
+            }, 2000)
+
+
+            console.log("errado");
+
+        }
+        lastCard = -1;
+        return;
+    }
+
+    lastCard = position;
+
+}
+
+
+
+/*
 let lastCard = -1; //posição -1 (inexistente) do container
-function showCard(event) {
 
+function clickCard(event) {
+    console.log(event)
+   
     //acessar a cardDiv (que é o 1º elemento filho do event)
-    const cardDiv = event.path[1];
-
+    const cardDiv = event.path[0];
+    cardDiv.classList.toggle("face");
+  
     //atribuir a posição da carta no container/tabuleiro (que será usada para comparação)
     const position = cardDiv.getAttribute("data-position");
 
-    flipCard(position, false);
+    flipCard(position, true);
 
     if (lastCard !== -1 && position !== lastCard) {
 
@@ -93,8 +146,11 @@ function showCard(event) {
             alert("ganhei um ponto");
 
         } else {
-            flipCard(position, true);
-            flipCard(lastCard, true);
+
+            flipCard(position, false);
+
+            flipCard(lastCard, false);
+
             alert("errado");
 
         }
@@ -106,27 +162,28 @@ function showCard(event) {
 
 }
 
-function flipCard(posicao, unflip) {
 
-    const cardDiv = document.querySelectorAll(".card")[posicao];
+function flipCard(position, show) {
 
-    cardDiv.classList.toggle("flip")
+    const cardDiv = document.querySelectorAll(".card")[position];
+
+
     //atribuindo o front-card à variável cardFront
     const cardFront = cardDiv.querySelector(".front-card");
-
 
     //atribuindo a back-card à variável cardBack
     const cardBack = cardDiv.querySelector(".back-card");
 
-    if (unflip == true) {
+    if (show == false) {
         //removendo a classe hidden da cardBack
-        cardBack.classList.add("hidden");
+        cardBack.classList.toggle("back-card");
 
         //adicionando a classe hidden à front-card
-        cardFront.classList.remove("hidden");
+        cardFront.classList.toggle("front-card");
 
     } else {
-        cardBack.classList.remove("hidden");
-        cardFront.classList.add("hidden");
+        cardBack.classList.toggle("back-card");
+        cardFront.classList.toggle("front-card");
     }
 }
+*/
